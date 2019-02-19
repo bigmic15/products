bf9f8a3e102d28aa00b08d541797f1eff74de966
Brian Kuan <bigmic15@gmail.com>
2019-02-19 15:54:20 +0800

    refactor

--- products.py
+++ products.py
@@ -1,32 +1,46 @@
 import os
 
-# To read the products.csv
-products = []
-if os.path.isfile('products.csv'): # To check for existence of the file
-	with open('products.csv', 'r', encoding = 'utf-8') as f:
-		for line in f:
-			if 'Name,Price' in line:
-				continue
-			name, price = line.strip().split(',')
-			products.append([name, price])
-	print(products)
+# To read a file
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if 'Name,Price' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    return products
 
 # User input 
-while True:
-	name = input('Please enter a product name: ')
-	if name == 'quit':
-		break
-	price = input('Please enter its price: ')
-	price = int(price)
-	products.append([name, price])
-print(products)	
+def user_input(products):
+    while True:
+        name = input('Please enter a product name: ')
+        if name == 'quit':
+            break
+        price = input('Please enter its price: ')
+        price = int(price)
+        products.append([name, price])
+    print(products)    
+    return products
 
 # To print all the records
-for p in products:
-	print(p[0], 'is $', p[1])
+def print_products(products):
+    for p in products:
+        print(p[0], 'is $', p[1])
 
-# To write the products.csv file
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('Name,Price\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+# To write a file
+def write_file(filename, products):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write('Name,Price\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename): # To check for existence of the file
+        products = read_file(filename)
+    products = user_input(products)
+    print_products(products)
+    write_file(filename, products)
+
+main()
